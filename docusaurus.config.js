26789c2300b7e420e638687c4434106b36a754a1
Patitotective <cristobalriaga@gmail.com>
2021-11-26 19:23:12 -0500

    Added image to be sent when you send the link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: 'img/logo.svg',
       colorMode: {
         defaultMode: "dark", 
       },       
