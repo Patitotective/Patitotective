819c069a48145d0004def6d9ae40e55a58ef2865
Patitotective <cristobalriaga@gmail.com>
2022-09-06 18:43:33 -0500

    Added ImNotify

--- readme.nim
+++ readme.nim
@@ -18,6 +18,7 @@ const data: seq[(string, seq[(string, Text)])] = @{
     "downit": ("https://github.com/Patitotective/downit", "asynchronous downloads manager"), 
     "ImStyle": ("https://github.com/Patitotective/ImStyle", "dear imgui styles manager"), 
     "tinydialogs": ("https://github.com/Patitotective/tinydialogs", "tinyfiledialogs bindings for nim"), 
+    "ImNotify": ("https://github.com/Patitotective/tinydialogs", "dear imgui notifications"), 
   }, 
   "🔬 Experiments": @{
     "ImNimble": ("https://github.com/Patitotective/ImNimble", "dear imgui nimble client application"), 
